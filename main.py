b6501f69ae6f88e3697ca98f72e340d0da81a39a
Luketss <lucassilluzio@gmail.com>
2021-01-13 19:34:44 -0300

    trocar dps

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+from datetime import datetime
 
 class Csv:
 
@@ -6,26 +7,44 @@ class Csv:
         self.__file_path = file_path
 
     def extract_csv_data(self):
+
+        data = []
         close_price = []
         open_price = []
+        min_value = []
+        max_price = []
+
         with open(self.__file_path) as csvfile:
             spamreader = csv.reader(csvfile)
             for row in spamreader:
-                if 'NaN' not in row:
-                    open_price.append(row[1])
-                    close_price.append(row[2])
-                    #print(', '.join(row))
+                if datetime.fromtimestamp(row[0][:9]) >= '2012-01-01' and datetime.fromtimestamp(row[0][:9]) <= '2018-01-01':
+                    if 'NaN' not in row:
+                        open_price.append(row[1])
+                        close_price.append(row[2])
+                        min_value.append(row[3])
+                        max_value.append(row[4])
+                        #print(', '.join(row))
+
         return open_price, close_price
+
+    def transform_timestamp_to_data(self):
+        pass
 
 class Candle:
     
-    def __init__(self, open_value, close_value):
+    def __init__(self, period, open_value, close_value, max_value, min_value):
+        self.period = period
         self.open_value = open_value
         self.close_value = close_value
+        self.max_value = max_value
+        self.min_value = min_value
+    
+    def draw_candle_graph(self):
+        pass
 
 class Indicador:
 
-    def __init__(self, numero_periodos, desvio_padrao=0)
+    def __init__(self, numero_periodos, desvio_padrao=0):
         self.numero_periodos = numero_periodos
         self.desvio_padrao = desvio_padrao
     
